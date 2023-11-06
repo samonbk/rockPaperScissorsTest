eb0de3de7ddfb03a7e0c6f759c7de3c5fd0f4ed7
brothong2000 <136555363+samonbk@users.noreply.github.com>
2023-11-06 14:39:32 +0700

    comit2

--- application.59cda.js
+++ application.59cda.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.167e8.json';
+          this.settingsPath = 'src/settings.41ab0.json';
           this.showFPS = false;
         }
 
--- index.a7340.js
+++ index.a7340.js
@@ -1,4 +1,4 @@
-System.register(["./application.bcfc0.js"], function (_export, _context) {
+System.register(["./application.59cda.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.bcfc0.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationBcfc0Js) {
-      Application = _applicationBcfc0Js.Application;
+    setters: [function (_application59cdaJs) {
+      Application = _application59cdaJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
